9542a9dfab29abdbf6c4d7f1b610bbd998fdb208
arasidprojectz <47368906+arasidprojectz@users.noreply.github.com>
2019-03-05 07:59:31 -0600

    Mar 5 - Edited at Home

--- Scene/sketch.js
+++ Scene/sketch.js
@@ -6,22 +6,41 @@
 // Add music, try to make a mouse cursor"
 
 let bgImage;
+let appleImg;
+let bananaImg; 
+let strawberryImg;
+let pineappleImg;
+let grapeImg;
+
+let scaleHat; 
+let scaleApple;
+let scalePineapple;
+let scaleStrawberry; 
+let scaleBanana; 
+let scaleGrape;
+
 let posX; 
 let posY; 
-let scaleHat; 
-let scaleFruit;
 let HatImg;
-let appleImg;
-let imgY = 0;
+
+let imgY;
+let x;
+let y;
 
 
 function setup() {
   bgImage = loadImage("assets/forestbackground.jpg")
   createCanvas(900, 700); 
   scaleHat = 0.09;
-  scaleFruit = 0.02;
+  scaleApple = 0.02;
+  scaleBanana = 0.015;
+  scaleGrape = 0.12;
+  scalePineapple = 0.1;
+  scaleStrawberry = 0.06;
   posX = 350;
   posY = 520;
+  imgY = 0;
+  x = random(width);
 }
 
 function draw() {
@@ -29,9 +48,14 @@ function draw() {
   displayHat();
   moveHat();
   createCursor();
-  // displayFruits();
+  displayFruits();
   // moveFruit();
 } 
+
+// Resizing the window
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight); 
+
 
 //Display the hat on the screen
 function displayHat() { 
@@ -60,17 +84,37 @@ function moveHat() {
 
 // Display the fruits images
 function displayFruits() {
-  image(appleImg, 0, imgY, appleImg.width * scaleFruit, appleImg.height * scaleFruit);
+  for (let a = 0; a < 15; a++) {
+    image(appleImg, x, imgY, appleImg.width * scaleApple, appleImg.height * scaleApple);
+  }
+  for (let b = 0; b < 5; b++) {
+    image(bananaImg, x, imgY, bananaImg.width * scaleBanana, bananaImg.height * scaleBanana);
+  }
+  for (let s = 0; s < 20; s++) {
+    image(strawberryImg, x, imgY, strawberryImg.width * scaleStrawberry, strawberryImg.height * scaleStrawberry);
+  }
+  for (let p = 0; p < 25; p++) {
+    image(pineappleImg, x, imgY, pineappleImg.width * scalePineapple, pineappleImg.height * scalePineapple);
+  }
+  for (let g = 0; g < 25; g++) {
+    image(grapeImg, x, imgY, grapeImg.width * scaleGrape, grapeImg.height * scaleGrape);
+  }
 }
 
-// Make fruits fall from the top of the screen
+// Make fruits fall down randomly from the top of the screen
 function moveFruit() {
   imgY += 1;
+ 
+
 }
 
 function preload() { 
   HatImg = loadImage("assets/hat.png");
-  appleImg = loadImage("assets/apple.png");
+  appleImg = loadImage("assets/appleclipart.png");
+  bananaImg = loadImage("assets/bananaclipart.png");
+  strawberryImg = loadImage("assets/strawberryclipart.png");
+  pineappleImg = loadImage("assets/pineappleclipart.png");
+  grapeImg = loadImage("assets/grapeclipart.png");
 }
 
 function createCursor() {
