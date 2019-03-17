4edd20623b3598f8a099f54b1a951abb9025cfe3
arasidprojectz <47368906+arasidprojectz@users.noreply.github.com>
2019-03-16 22:35:54 -0600

    Mar 16 - Scene Edited

--- Scene/sketch.js
+++ Scene/sketch.js
@@ -5,18 +5,18 @@
 // Extra for Experts:
 // Add music, try to make a mouse cursor
 
-let bgImage, basketImg, appleImg, bananaImg, strawberryImg, pineappleImg, grapeImg, scoreButtonImg, grenadeImg;
-let basketSize, appleSize, bananaSize, pineappleSize, grapeSize, strawberryWidth, strawberryHeight, scoreButtonWidth, scoreButtonHeight, grenadeSize;
-let appleX, bananaX, pineappleX, grapeX, strawberryX, buttonX, grenadeX; 
-let appleY, bananaY, pineappleY, grapeY, strawberryY, buttonY, grenadeY;
+let bgImage, basketImg, appleImg, bananaImg, strawberryImg, pineappleImg, grapeImg, scoreButtonImg, grenadeImg, grenadeImg2, nuclearImg;
+let basketSize, appleSize, bananaSize, pineappleSize, grapeSize, strawberryWidth, strawberryHeight, scoreButtonWidth, scoreButtonHeight, grenadeSize, grenadeSize2 , nuclearSize;
+let appleX, bananaX, pineappleX, grapeX, strawberryX, buttonX, grenadeX, grenadeX2, nuclearX; 
+let appleY, bananaY, pineappleY, grapeY, strawberryY, buttonY, grenadeY, grenadeY2, nuclearY;
 let posX, posY, cursorX, cursorY, xSpeed, hit, gamePoint;
 
 function setup() {
   bgImage = loadImage("assets/forestbackground.jpg");
   createCanvas(900, 700); 
-  basketSize = 90, appleSize = 35, bananaSize = 50, grapeSize = 45, pineappleSize = 60, strawberryWidth = 30, strawberryHeight = 35, scoreButtonWidth = 100, scoreButtonHeight = 50, grenadeSize = 60;
-  appleX = random(0, width), bananaX = random(5, width), pineappleX = random(10, width), grapeX = random(15, width), strawberryX = random(20, width), grenadeX = random(20, width), buttonX = 735;
-  appleY = 0, bananaY = 0, pineappleY = 0, grapeY = 0, strawberryY = 0, buttonY = 0, grenadeY = 0;
+  basketSize = 90, appleSize = 35, bananaSize = 50, grapeSize = 45, pineappleSize = 60, strawberryWidth = 30, strawberryHeight = 35, scoreButtonWidth = 100, scoreButtonHeight = 50, grenadeSize = 60, grenadeSize = 60, grenadeSize2 = 80, nuclearSize = 70;
+  appleX = random(0, 880), bananaX = random(0, 880), pineappleX = random(0, 880), grapeX = random(0, 880), strawberryX = random(0, 880), grenadeX = random(0, 890), grenadeX2 = random(0, 890), nuclearX = random(0, 890), buttonX = 735;
+  appleY = 0, bananaY = 0, pineappleY = 0, grapeY = 0, strawberryY = 0, buttonY = 0, grenadeY = 0, grenadeY2 = 0, nuclearY = 0;
   posX = 350, posY = 520, cursorX = 100, cursorY = 100, xSpeed = 5, hit = false, gamePoint = 0;
 }
 
@@ -46,6 +46,26 @@ function pointForGrenade() {
   textLeading(10); 
   text(grenadePoint, grenadeX, grenadeY);
   gamePoint -= 15;
+}
+
+function pointForGrenade2() {
+  let grenadePoint2 = "-30";
+  fill(0);
+  noStroke();
+  textSize(25);
+  textLeading(10); 
+  text(grenadePoint2, grenadeX2, grenadeY2);
+  gamePoint -= 30;
+}
+
+function pointForNuclear() {
+  let grenadePoint = "-100";
+  fill(0);
+  noStroke();
+  textSize(25);
+  textLeading(10); 
+  text(grenadePoint, grenadeX, grenadeY);
+  gamePoint -= 100;
 }
 function pointForApple() {
   let applePoint = "+6";
@@ -97,8 +117,6 @@ function pointForStrawberry() {
   gamePoint += 8;
 }
 
-
-
 function checkCollide() {
   hit = collideRectRect(appleX, appleY, appleSize, appleSize, posX, posY, basketSize, basketSize);
   if (hit === true) {
@@ -123,6 +141,14 @@ function checkCollide() {
   hit = collideRectRect(grenadeX, grenadeY, grenadeSize, grenadeSize, posX, posY, basketSize, basketSize);
   if (hit === true) {
     pointForGrenade();  
+  } 
+  hit = collideRectRect(grenadeX2, grenadeY2, grenadeSize2, grenadeSize2, posX, posY, basketSize, basketSize);
+  if (hit === true) {
+    pointForGrenade2();  
+  }
+  hit = collideRectRect(nuclearX, nuclearY, nuclearSize, nuclearSize, posX, posY, basketSize, basketSize);
+  if (hit === true) {
+    pointForNuclear();  
   }
 }
 
@@ -175,6 +201,8 @@ function displayImg() {
   image(grapeImg, grapeX, grapeY, grapeSize, grapeSize);
   image(scoreButtonImg, buttonX, buttonY, scoreButtonWidth, scoreButtonHeight);
   image(grenadeImg, grenadeX, grenadeY, grenadeSize, grenadeSize);
+  image(grenadeImg2, grenadeX2, grenadeY2, grenadeSize2, grenadeSize2);
+  image(nuclearImg, nuclearX, nuclearY, nuclearSize, nuclearSize);
 }
 
 // Make fruits fall down randomly from the top of the screen
@@ -182,37 +210,49 @@ function moveFruit() {
   appleY += random(1, 1.5);
   if (appleY > 486) {
     appleY = 0;
-    appleX = random(0, width);
+    appleX = random(0, 880);
   }
 
   bananaY += random(1, 1.9);
   if (bananaY > 487) {
     bananaY = 0;
-    bananaX = random(5, width);
+    bananaX = random(0, 880);
   }
 
   pineappleY += random(1, 2.3);
   if (pineappleY > 487) {
     pineappleY = 0;
-    pineappleX = random(10, width);
+    pineappleX = random(0, 880);
   }
 
   grapeY += random(1, 2.7);
   if (grapeY > 488) {
     grapeY = 0;
-    grapeX = random(15, width);
+    grapeX = random(0, 880);
   }
 
   strawberryY += random(1, 2.9);
   if (strawberryY > 486.6) {
     strawberryY = 0;
-    strawberryX = random(20, width);
+    strawberryX = random(0, 880);
   }
 
-  grenadeY += random(1, 3.5);
+  grenadeY += random(1, 3);
   if (grenadeY > 462) {
     grenadeY = 0;
-    grenadeX = random(20, width);
+    grenadeX = random(0, 890);
+  }
+
+  grenadeY2 += random(1, 3.5);
+  if (grenadeY2 > 462) {
+    grenadeY2 = 0;
+    grenadeX2 = random(0, 890);
+  }
+
+  nuclearY += random(1, 3.5);
+  if (nuclearY > 462) {
+    nuclearY = 0;
+    nuclearX = random(0, 890);
   }
 }
 
@@ -220,7 +260,8 @@ function preload() {
   basketImg = loadImage("assets/basket-clipart.png "), appleImg = loadImage("assets/appleclipart.png");
   bananaImg = loadImage("assets/bananaclipart.png"), strawberryImg = loadImage("assets/strawberryclipart.png");
   pineappleImg = loadImage("assets/pineappleclipart.png"), grapeImg = loadImage("assets/grapeclipart.png");
-  scoreButtonImg = loadImage("assets/score-button.png"); grenadeImg = loadImage("assets/grenade.png");
+  scoreButtonImg = loadImage("assets/score-button.png"); grenadeImg = loadImage("assets/grenade-clipart.png");
+  grenadeImg2 = loadImage("assets/grenade-clipart2.png"); nuclearImg = loadImage("assets/nuclear-bomb.png");
 }
 
 function createCursor() {
