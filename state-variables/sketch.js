6eb82cd24f7c42737aedd449e59b0b1bd9ffe083
arasidprojectz <alrasidma@gmail.com>
2019-03-18 15:20:15 -0600

    Create a New Folder - State Variable

---
+++ state-variables/sketch.js
@@ -0,0 +1,15 @@
+// State Variables Assignment
+// Al Rasid Mamun
+// March 25, 2019
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
