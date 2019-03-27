ba3598acf164c69361d38fa689d89fa385747bd2
arasidprojectz <alrasidma@gmail.com>
2019-03-27 15:19:51 -0600

    Add more

--- perlin-noise/sketch.js
+++ perlin-noise/sketch.js
@@ -3,7 +3,6 @@
 let time;
 let rectWidth;
 let numberOfRects;
-let myRectangle;
 let rects = [];
 
 function setup() {
@@ -31,8 +30,6 @@ function generateInitialTerrain() {
 
 function draw() {
   background(220);
-
-  time += 0.01; 
   fill(0);
   for (let i = 0; i < numberOfRects; i ++) {
     rect(rect[i].x, rect[i].y, rect[i].width, rect[i].height);
