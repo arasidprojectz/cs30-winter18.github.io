e6c971c661b8f56dd5e7c7b6816fede4eb0c2fab
arasidprojectz <alrasidma@gmail.com>
2019-03-28 14:43:02 -0600

    Add to Perlin Noise

--- perlin-noise/sketch.js
+++ perlin-noise/sketch.js
@@ -9,30 +9,53 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   numberOfRects = width;
   time = 0;
-  rectWidth = width/numberOfRects;
+  rectWidth = width / numberOfRects;
   generateInitialTerrain();
 }
 
+function draw() {
+  background(220);
+  fill(0);
+
+  //remove leftmost rectangle
+  rects.shift();
+
+  //add rectangle on right
+  let rectHeight = noise(time) * height;
+  let myRectangle = {
+    height: rectHeight,
+    width: rectWidth,
+    x: width - rectWidth,
+    y: height - rectHeight,
+  };
+  rects.push(myRectangle);
+
+  // move along the perlin noise x-axis
+  time += 0.001;
+
+
+  //display the terrain
+  for (let i = 0; i < rects.length; i ++) {
+    //move rect to the left 
+    rects[i].x -= rectWidth;
+    rect(rects[i].x, rects[i].y, rects[i].width, rects[i].height);
+  }
+  
+}
+
 function generateInitialTerrain() {
-  for (let i = 0, i < numberOfRects; i ++) {
+  for (let i = 0; i < numberOfRects; i++) {
     let rectHeight = noise(time) * height;
     let myRectangle = {
       height: rectHeight,
       width: rectWidth,
       x: i * rectWidth,
       y: height - rectHeight,
+    };
+    rects.push(myRectangle);
+
+    // move along the perlin noise x-axis
+    time += 0.001;
   }
-  rects.push(myRectangle); 
-  //move along the perlin noise x-axis
-  time += 0.1;
   
-}
-
-function draw() {
-  background(220);
-  fill(0);
-  for (let i = 0; i < numberOfRects; i ++) {
-    rect(rect[i].x, rect[i].y, rect[i].width, rect[i].height);
-
-  }
 }
