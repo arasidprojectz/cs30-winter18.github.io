d606ac4306d00e45c521f367dec226b79d9817f4
arasidprojectz <alrasidma@gmail.com>
2019-04-03 14:46:52 -0600

    Updated Game of Life

--- grid/sketch.js
+++ grid/sketch.js
@@ -2,12 +2,19 @@
 // Dan Schellenberg
 // April 1, 2019
 
-let gridSize = 10;
+let gridSize = 50;
 let grid;
 let cellSize;
+let autoPlay;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  if (windowWidth > windowHeight) {
+    createCanvas(windowHeight, windowHeight);
+  }
+  else {
+    createCanvas(windowWidth, windowWidth);
+  }
+  
   grid = createRandom2DArray(gridSize, gridSize);
   cellSize = width/gridSize;
 }
@@ -43,45 +50,6 @@ function create2DArray(cols, rows) {
   return emptyArray;
 }
 
-function update(){
-  let nextTurn = create2DArray(gridSize, gridSize);
-
-  for (let y = 0; y < gridSize; y++) {
-    for(let x = 0; x < gridSize; x++) {
-      let neighbors = 0;
-
-      //look at the 3x3 grid around the current location
-
-      for (let i = -1; i < 1; i++) {
-        for (let j = -1; j <= 1; j++) {
-          if (y+i >= 0 && y+i < gridSize && x+j >= 0 && x+j < gridSize){
-            neighbors += grid[y+i][x+j];
-          }
-        }
-      }
-    }
-
-    neighbors -= grid[y][x];
-
-    //applying the rules of the game
-    if (grid[y][x] == 1) {
-      if (neighbors === 2 || neighbors === 3) {
-        nextTurn[y][x] = 1;
-      }
-      else {
-        nextTurn[y][x] = 0;
-      }
-    }
-  }
-
-  grid = nextTurn;
-}
-
-function keypressed() {
-  if (key === " ") {
-    update();  } 
-}
-
 function createRandom2DArray(cols, rows) {
   let emptyArray = [];
   for (let i = 0; i < rows; i++) {
@@ -98,13 +66,84 @@ function createRandom2DArray(cols, rows) {
   return emptyArray;
 }
 
-function mousePressed(){ 
-  let xcord = floor(mouseX / cellSize);
-  let ycord = floor(mouseY / cellSize);
+function update() {
+  let nextTurn = create2DArray(gridSize, gridSize);
 
-  if (grid[ycord][xcord] === 1) {
+  for (let y = 0; y < gridSize; y++) {
+    for (let x = 0; x < gridSize; x++) {
+      let neighbors = 0;
 
+      //look at the 3x3 grid around the current location
+      for (let i = -1; i <= 1; i++) {
+        for (let j = -1; j <= 1; j++) {
+          if (y+i >= 0 && y+i < gridSize && x+j >= 0 && x+j < gridSize) {
+            neighbors += grid[y+i][x+j];
+          }
+        }
+      }
+
+      neighbors -= grid[y][x];
+
+      //applying the rules of the game
+      if (grid[y][x] === 1) { //alive
+        if (neighbors === 2 || neighbors === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+
+      if (grid[y][x] === 0) { //dead
+        if (neighbors === 3) {
+          nextTurn[y][x] = 1;
+        }
+        else {
+          nextTurn[y][x] = 0;
+        }
+      }
+    }
   }
 
+  grid = nextTurn;
+}
 
+function keyPressed() {
+  if (key === " ") { 
+    window.clearInterval(autoPlay);
+    update();
+  }
+  if (key === "c") {
+    grid = create2DArray(gridSize, gridSize);
+  }
+  if (key === "r") {
+    grid = createRandom2DArray(gridSize, gridSize);
+  }
+
+  if (key === "a") {
+    autoPlay = window.setInterval(update, 50);
+  }
+  if (key === "s") {
+    saveJSON(grid, "thegrid.json");
+  }
+  if (key === "g") {
+    noLoop();
+    grid = loadJSON("assets/thegrid.json", loadingComplete);
+  }
+}
+
+function loadingComplete() {
+  loop();
+}
+
+function mousePressed() {
+  let xcoord = floor(mouseX / cellSize);
+  let ycoord = floor(mouseY / cellSize);
+
+  if (grid[ycoord][xcoord] === 1) {
+    grid[ycoord][xcoord] = 0;
+  }
+  else {
+    grid[ycoord][xcoord] = 1;
+  }
 }
