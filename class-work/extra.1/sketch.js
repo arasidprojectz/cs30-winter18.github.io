ee34b1ca49c8a51f60d8e16dc3fb9cb3609b360a
arasidprojectz <alrasidma@gmail.com>
2019-04-12 14:12:05 -0600

    Make Some Changes

--- class-work/extra.1/sketch.js
+++ class-work/extra.1/sketch.js
@@ -5,15 +5,16 @@ let x;
 let y;
 let speedX;
 let speedY;
-
+stateOfWalk;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   index = 0;
-  x = 0;
+  x = width/2;
   y = 0;
   speedX = 10;
   speedY = 1; 
+  stateOfWalk = "left";
 }
 
 function preload() {
@@ -21,39 +22,50 @@ function preload() {
     imgs[i] = loadImage("assets/m1-movelt/moveleft"+i+".png");
 
   }
-
+  for (let e=0; e<5; e++) {
+    imgs2[e] = loadImage("assets/m1-movert/m1-movert"+e+".png");
+  }  
 
 }
 
 function draw() {
   background(0);
-  image(imgs[index], x, y, 200, 220);
+  checkState();
+
+
   // image(imgs2[index], x, y, 200, 220);
   // moveLeft();
-  // image(imgs[index], 0, 0, 200, 220);
-  // index = (index + 1) % imgs.length;
-  // for (let i=0; i<5; i++) {
-  //   image(imgs[i], 0, 0, 140, 160); 
-  //   i++;
-  // }
+}
 
+
+
+function checkState() {
+  if (stateOfWalk === "left") {
+    image(imgs[index], x, y, 200, 220);
+  }
+  if (stateOfWalk === "right") {
+    image(imgs2[index], x, y, 200, 220);
+  }
 }
 
 function keyPressed() {
   if (keyCode === RIGHT_ARROW) {
+    stateOfWalk = "right";
     index = (index + 1) % imgs.length;
     x += speedX;
-  } else if (keyCode === RIGHT_ARROW) {
-    index = (index + 1) % imgs.length;
+  } else if (keyCode === LEFT_ARROW) {
+   stateOfWalk === "left";
+   index = (index + 1) % imgs.length;
+    x -= speedX;
   }
 }
 
-function moveLeft() {
-  if (keyIsDown(RIGHT_ARROW)) {
-    index = (index + 1) % imgs.length;
-    x += speedX;
-  }
-}
+// function moveLeft() {
+//   if (keyIsDown(RIGHT_ARROW)) {
+//     index = (index + 1) % imgs.length;
+//     x += speedX;
+//   }
+// }
 
 // function keyTyped() {
 //   if (key === 'RIGHT_ARROW') {
