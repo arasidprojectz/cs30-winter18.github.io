7a1b1aa91f41b2e9050001bf35f353cd875693dc
arasidprojectz <alrasidma@gmail.com>
2019-04-12 15:20:41 -0600

    Find a way to move the sprite

--- class-work/extra.1/sketch.js
+++ class-work/extra.1/sketch.js
@@ -1,76 +1,46 @@
-let imgs = [];
-let imgs2 = [];
-let index;
+let imgs;
+let imgs1;
 let x;
 let y;
-let speedX;
-let speedY;
-stateOfWalk;
+let state;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  index = 0;
-  x = width/2;
+  x = 0;
   y = 0;
-  speedX = 10;
-  speedY = 1; 
-  stateOfWalk = "left";
+  state = 1;
+
 }
 
 function preload() {
-  for (let i=0; i<5; i++) {
-    imgs[i] = loadImage("assets/m1-movelt/moveleft"+i+".png");
+  imgs = loadImage("assets/m1-movelt/moveleft0.png");
+  imgs1 = loadImage("assets/m1-movert/m1-movert0.png");
 
-  }
-  for (let e=0; e<5; e++) {
-    imgs2[e] = loadImage("assets/m1-movert/m1-movert"+e+".png");
-  }  
 
 }
 
 function draw() {
   background(0);
-  checkState();
+  checkState()
+  if (state === 1) {
+    image(imgs, x, y, 200, 220);
+  }
+  if (state === 2) {
+    image(imgs1, x, y, 200, 220);
+  }
+    
 
-
-  // image(imgs2[index], x, y, 200, 220);
-  // moveLeft();
-}
-
-
+} 
 
 function checkState() {
-  if (stateOfWalk === "left") {
-    image(imgs[index], x, y, 200, 220);
-  }
-  if (stateOfWalk === "right") {
-    image(imgs2[index], x, y, 200, 220);
-  }
-}
-
-function keyPressed() {
-  if (keyCode === RIGHT_ARROW) {
-    stateOfWalk = "right";
-    index = (index + 1) % imgs.length;
-    x += speedX;
-  } else if (keyCode === LEFT_ARROW) {
-   stateOfWalk === "left";
-   index = (index + 1) % imgs.length;
-    x -= speedX;
+  if (keyIsDown(RIGHT_ARROW)) {
+    state = 1;
+    x += 5;
+  } 
+  else if (keyIsDown(LEFT_ARROW)) {
+    state = 2;
+    x -= 5;
   }
 }
 
-// function moveLeft() {
-//   if (keyIsDown(RIGHT_ARROW)) {
-//     index = (index + 1) % imgs.length;
-//     x += speedX;
-//   }
-// }
-
-// function keyTyped() {
-//   if (key === 'RIGHT_ARROW') {
-//     index = (index + 1) % imgs.length;
-//   }
-// }
-
-    
--- class-work/extra/sketch.js
+++ class-work/extra/sketch.js
@@ -5,10 +5,12 @@ let x;
 let y;
 let speedX;
 let speedY;
+let state;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  state = 1;
   index = 0;
   x = 0;
   y = 0;
@@ -19,37 +21,49 @@ function setup() {
 function preload() {
   for (let i=0; i<5; i++) {
     imgs[i] = loadImage("assets/m1-movelt/moveleft"+i+".png");
-
   }
-
-
+  for (let i=0; i<5; i++) {
+    imgs[i] = loadImage("assets/m1-movelt/moveleft"+i+".png")
+  }
 }
 
 function draw() {
   background(0);
-  image(imgs[index], x, y, 200, 220);
-  // image(imgs2[index], x, y, 200, 220);
-  // moveLeft();
-  // image(imgs[index], 0, 0, 200, 220);
-  // index = (index + 1) % imgs.length;
-  // for (let i=0; i<5; i++) {
-  //   image(imgs[i], 0, 0, 140, 160); 
-  //   i++;
-  // }
-
-}
-
-function keyPressed() {
-  if (keyCode === RIGHT_ARROW) {
-    index = (index + 1) % imgs.length;
-    x += speedX;
-  } else if (keyCode === RIGHT_ARROW) {
-    index = (index + 1) % imgs.length;
+  if (state = 1) {
+    image(imgs[index], x, y, 200, 220);
   }
+  if (state = 2) {
+    image(imgs[index], x, y, 200, 220);
+  }
+  // image(imgs2[index], x, y, 200, 220);
+  moveLeft();
+
 }
+
+if (state === 1) {
+  image(imgs, x, y, 200, 220);
+}
+if (state === 2) {
+  image(imgs1, x, y, 200, 220);
+}
+
+// function keyPressed() {
+//   if (keyCode === RIGHT_ARROW) {
+//     index = (index + 1) % imgs.length;
+//     x += speedX;
+//   } else if (keyCode === RIGHT_ARROW) {
+//     index = (index + 1) % imgs.length;
+//   }
+// }
 
 function moveLeft() {
-  if (keyIsDown(RIGHT_ARROW)) {
+  if (keyIsDown(RIGHT_ARROW) && frameCount % 5 === 0) {
+    state = 1;
+    index = (index + 1) % imgs.length;
+    x += speedX;
+  }
+  else if (keyIsDown(LEFT_ARROW) && frameCount % 5 === 0) {
+    state = 2;
     index = (index + 1) % imgs.length;
     x += speedX;
   }
