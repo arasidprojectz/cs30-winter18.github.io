04eb7cc6099d4100b09e602eeefd0291e7b09a19
arasidprojectz <alrasidma@gmail.com>
2019-04-17 14:38:45 -0600

    Change File Name

--- class-work/extra-of-jumping/sketch.js
+++ class-work/extra-of-jumping/sketch.js
@@ -2,7 +2,7 @@
 let y, speedY;
 let x, radius;
 let gravity, jumping;
-let imgs = [], imgs2 = [], imgWidth, imgHeight, ground, index, stateOfJump;
+let imgs = [], imgs2 = [], imgWidth, imgHeight, ground, index, state;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -15,29 +15,35 @@ function setup() {
   speedY = 0;
   jumping = true;
   index = 0;
-  stateOfJump = 1;
+  state = 2;
 }
 
 function draw() {
   background(220);
+  stateCheck(); 
   y += speedY;
-
   speedY += gravity;
   if (y > ground) {
-    image(imgs[index], x, y, imgWidth, imgHeight);
     jumping = false;
     y = ground;
   }
   // index = (index += 1) % imgs.length;
   // image(imgs[index], x, y, imgWidth, imgHeight);
-  image(imgs2[index], x, y, imgWidth, imgHeight);
+//   image(imgs2[index], x, y, imgWidth, imgHeight);
 }
 
+function stateCheck() {
+  if (state === 1) {
+    image(imgs[index], x, y, imgWidth, imgHeight);
+  } 
+  if (state === 2) {
+    image(imgs2[index], x, y, imgWidth, imgHeight);
+  } 
+}
 
 function keyPressed() {
-  if ((key === " ") && jumping === false)  {
-    // index = (index += 1) % imgs.length;
-    image(imgs2[index], x, y, imgWidth, imgHeight);
+  if ((key === " ") && jumping === false) {
+    index = (index + 1) % imgs.length;
     speedY = -5;
     jumping = true;
     
@@ -47,6 +53,7 @@ function keyPressed() {
 function preload() {
   for (let i=0; i<2; i++) {
     imgs[i] = loadImage("assets/m1-jumplt/state-true/m1-jumplt"+i+".png");
-    imgs2[i] = loadImage("assets/m1-jumplt/state-true/m1-jumplt"+i+".png");  }
+    imgs2[i] = loadImage("assets/m1-jumplt/state-true/m1-jumplt"+i+".png");  
+  }
   
 }
