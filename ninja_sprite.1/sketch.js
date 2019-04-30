a5ad7727d120b9e5ee9ef48a1252a0871c0797e9
arasidprojectz <47368906+arasidprojectz@users.noreply.github.com>
2019-04-30 13:13:06 -0600

    Add/Remove Image

--- ninja_sprite.1/sketch.js
+++ ninja_sprite.1/sketch.js
@@ -1,59 +1,25 @@
-let imgs = [];
-let imgs2 = [];
-let index;
+let imgs;
 let x;
 let y;
-let speedX;
-let speedY;
-let state;
+
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  state = 1;
-  index = 0;
   x = 0;
   y = 0;
-  speedX = 10;
-  speedY = 1; 
 }
 
 function preload() {
-  for (let i=0; i<5; i++) {
-    imgs[i] = loadImage("assets/ninja_walk/move_left/ninja_walk"+i+".png");
-    imgs2[i] = loadImage("assets/ninja_walk/move_right/ninja_walk"+i+".png");
-  }
+  imgs = loadImage("assets/ninja_walk/move_left/ninja_walk1.png");
 }
 
 function draw() {
-  background(0);
-  checkState();
-  moveLeft();
-
+  image(imgs, x, y, 240, 200);
 }
 
 
-function checkState() {
-  if (state === 1) {
-    image(imgs[index], x, y, 200, 220);
-  }
-  if (state === 2) {
-    image(imgs2[index], x, y, 200, 220);
-  }
-}
 
-function moveLeft() {
-  if (keyIsDown(RIGHT_ARROW) && frameCount % 12 === 0) {
-    state = 1;
-    index = (index + 1) % imgs.length;
-    x += speedX;
-  }
-  else if (keyIsDown(LEFT_ARROW) && frameCount % 12 === 0) {
-    state = 2;
-    index = (index + 1) % imgs.length;
-    x -= speedX;
-  }
-}
 
 
 // let y, speedY;
